063280c52245d9dcff5c16a4004f37eead4e776b
Chris Small <chsmall@uw.edu>
2013-10-25 18:13:48 -0700

    Created switches page

--- web/resonance/resonance/urls.py
+++ web/resonance/resonance/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, include, url
-from resonance.views import hello,overview
+from resonance.views import hello,overview,switches
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -16,5 +16,6 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     #url(r'^admin/', include(admin.site.urls)),
     url(r'^hello/$', hello),
-    url(r'^overview/$', overview)
+    url(r'^overview/$', overview),
+    url(r'^switches/$', switches),
 )
--- web/resonance/resonance/views.py
+++ web/resonance/resonance/views.py
@@ -11,3 +11,9 @@ def overview(request):
     t = get_template('overview.html')
     html = t.render(Context({'current_date': now}))
     return HttpResponse(html)
+
+def switches(request):
+    now = datetime.datetime.now()
+    t = get_template('switches.html')
+    html = t.render(Context({'current_date': now}))
+    return HttpResponse(html)
